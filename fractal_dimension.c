f14a2620a490a27875e5c9c7e5fbbd88116ffc9e
sferanchuk <39887187+sferanchuk@users.noreply.github.com>
2021-02-06 14:57:56 +0300

    Add files via upload

--- fractal_dimension.c
+++ fractal_dimension.c
@@ -28,10 +28,14 @@ void higuchi_dimension( double *data_x, double *data_y, int npoints, int method,
 {
     int i, j, k, npasses;
     int num_intervals, interval;
-    double dx, dy, partial_sum, t_slope, t_intercept;
+    double dx, dy, partial_sum;
     double path[ MAX_INTERVALS ], intervals[ MAX_INTERVALS ], log_path[ MAX_INTERVALS ], log_intervals[ MAX_INTERVALS ];
     
-    if ( npoints > 100 ) 
+    if ( npoints > 100000 ) 
+    { 
+        for ( i = ( ( interval_min == 0 ) ? 3 : interval_min ), num_intervals = 0; i < npoints && ( interval_max == 0 || i < interval_max ); i = i * 2, num_intervals++ ) intervals[ num_intervals] = i; 
+    }
+    else if ( npoints > 100 ) 
     { 
         for ( i = ( ( interval_min == 0 ) ? 2 : interval_min ), num_intervals = 0; i < npoints && ( interval_max == 0 || i < interval_max ); i = ( 3 * i ) / 2, num_intervals++ ) intervals[ num_intervals] = i; 
     }
@@ -62,7 +66,7 @@ void higuchi_dimension( double *data_x, double *data_y, int npoints, int method,
             log_intervals[i] = log( intervals[i] );
             log_path[i] = log( path[i] );
     }
-    *correlation = linear_regression( log_intervals, log_path, num_intervals, &t_slope, &t_intercept );
+    *correlation = linear_regression( log_intervals, log_path, num_intervals, slope, intercept );
     
     if ( extra_output )
     {
@@ -72,14 +76,18 @@ void higuchi_dimension( double *data_x, double *data_y, int npoints, int method,
     }
 }
 
-void peng_dimension( double *data_x, double *data_y, int npoints, int interval_min, int interval_max, int extra_output, double *slope, double *intercept, double *correlation )
+void peng_dimension( double *data_x, double *data_y, int npoints, int method, int interval_min, int interval_max, int extra_output, double *slope, double *intercept, double *correlation )
 {
     int i, j, k, npasses;
     int num_intervals, interval;
     double slice_slope, slice_intercept, dx, dy, partial_sum;
     double path[ MAX_INTERVALS ], intervals[ MAX_INTERVALS ], log_path[ MAX_INTERVALS ], log_intervals[ MAX_INTERVALS ];
   
-    if ( npoints > 100 ) 
+    if ( npoints > 100000 ) 
+    { 
+        for ( i = ( ( interval_min == 0 ) ? 3 : interval_min ), num_intervals = 0; i < npoints && ( interval_max == 0 || i < interval_max ); i = i * 2, num_intervals++ ) intervals[ num_intervals] = i; 
+    }
+    else if ( npoints > 100 ) 
     { 
         for ( i = ( ( interval_min == 0 ) ? 3 : interval_min ), num_intervals = 0; i < npoints && ( interval_max == 0 || i < interval_max ); i = ( 3 * i ) / 2, num_intervals++ ) intervals[ num_intervals] = i; 
     }
@@ -101,7 +109,7 @@ void peng_dimension( double *data_x, double *data_y, int npoints, int interval_m
                 dx = data_x[ interval * ( k + 1 ) + j ] - data_x[ interval * k + j ];
                 dy = dx * slice_slope;  
                 if ( dy == dy )
-                    partial_sum += ( dy > 0) ? dy : -dy;
+                    partial_sum += ( method == 0 ) ? ( ( dy > 0 ) ? dy : -dy ) : sqrt( dx * dx + dy * dy );
                 npasses++;
             }
         }
@@ -112,6 +120,7 @@ void peng_dimension( double *data_x, double *data_y, int npoints, int interval_m
             log_intervals[i] = log( intervals[i] );
             log_path[i] = log( path[i] );
     }
+    
     *correlation = linear_regression( log_intervals, log_path, num_intervals, slope, intercept );
     if ( extra_output )
     {
@@ -145,7 +154,7 @@ void logperiodic_intervals( int min_count, int num_peaks, int direction, double 
     }
 }
 
-int logperiodic_approximation( double *x_coord, double *y_coord, int num_points, int method, int interval_min, int interval_max, int extra_output, int *first_period, int *last_period, double *correlation )
+int logperiodic_approximation( double *x_coord, double *y_coord, int num_points, int method, int interval_min, int interval_max, int extra_output, int *first_period, int *last_period,  double *slope, double *intercept, double *correlation )
 {
     int i, j, k;
     int direction;
@@ -160,7 +169,7 @@ int logperiodic_approximation( double *x_coord, double *y_coord, int num_points,
     {
         for ( i = 2; i < 1000; i = ( i * 3 ) / 2 )
         {
-            if ( extra_output ) printf( "[min_period=%d,%s]", i, ( direction ) ? "forwards" : "backwards" );
+            if ( extra_output ) printf( "[min_period=%d,%s]\n", i, ( direction == 0 ) ? "forwards" : "backwards" );
             for ( j = 2; j < 100; j = ( j * 3 ) / 2 )
             {
                 peak_at_left = ( direction ) ? log( i + 1 ) : log( i + j );
@@ -172,25 +181,55 @@ int logperiodic_approximation( double *x_coord, double *y_coord, int num_points,
                             ( x_coord[0] + ( x_coord[k] - x_coord[0] ) * sum_of_intervals / ( peak_at_right - peak_at_left ) ) :
                             ( x_coord[ num_points - 1 ] - ( x_coord[ num_points - 1 ] - x_coord[k] ) * sum_of_intervals / ( peak_at_left - peak_at_right ) );
                 }
-                if ( method == 0 ) peng_dimension( updated_x_coord, y_coord, num_points, interval_min, interval_max, 0, &t_slope, &t_intercept, &t_correlation );
-                else higuchi_dimension( updated_x_coord, y_coord, num_points, 1, interval_min, interval_max, 0, &t_slope, &t_intercept, &t_correlation );
+                
+                if ( method == 0 ) 
+                    peng_dimension( updated_x_coord, y_coord, num_points, 1, interval_min, interval_max, 0, &t_slope, &t_intercept, &t_correlation );
+                else 
+                    higuchi_dimension( updated_x_coord, y_coord, num_points, 1, interval_min, interval_max, 0, &t_slope, &t_intercept, &t_correlation );
+                
                 if ( extra_output ) printf( " %d,%g", j, t_correlation );
+                
                 if ( t_correlation < min_correlation )
                 {
                     min_correlation = t_correlation;
                     *first_period = ( direction ) ? i : i + j;
                     *last_period = ( direction ) ? i + j : i;
-                    *correlation = min_correlation;
+                    *slope = t_slope;
+                    *intercept = t_intercept;
+                    *correlation = t_correlation;
                     
                 }
             }
             if ( extra_output ) printf( "\n" );
         }
     }
-    free( updated_x_coord );
 
-    if ( method == 0 ) peng_dimension( x_coord, y_coord, num_points, interval_min, interval_max, 0, &t_slope, &t_intercept, &t_correlation );
-    else higuchi_dimension( x_coord, y_coord, num_points, 1, interval_min, interval_max, 0, &t_slope, &t_intercept, &t_correlation );
+    if ( method == 0 ) peng_dimension( x_coord, y_coord, num_points, 1, interval_min, interval_max, extra_output, &t_slope, &t_intercept, &t_correlation );
+    else higuchi_dimension( x_coord, y_coord, num_points, 1, interval_min, interval_max, extra_output, &t_slope, &t_intercept, &t_correlation );
+
+    if ( extra_output )
+    {
+        direction = ( *last_period > *first_period );
+        i = ( direction ) ? *first_period : *last_period;
+        j = ( direction ) ? *last_period - i : *first_period - 1;last_period;
+
+        peak_at_left = ( direction ) ? log( i + 1 ) : log( i + j );
+        peak_at_right = ( direction ) ? log( i + j ) : log( i + 1 );
+        for ( k = 0; k < i; k++ )
+        {
+                sum_of_intervals =  log( i + (double)( k * j ) / num_points ) - log( i + 1 );
+                updated_x_coord[k] = ( k ) ?
+                    ( x_coord[0] + ( x_coord[k] - x_coord[0] ) * sum_of_intervals / ( peak_at_right - peak_at_left ) ) :
+                    ( x_coord[ num_points - 1 ] - ( x_coord[ num_points - 1 ] - x_coord[k] ) * sum_of_intervals / ( peak_at_left - peak_at_right ) );
+        }
+        
+        if ( method == 0 ) 
+            peng_dimension( updated_x_coord, y_coord, num_points, 1, interval_min, interval_max, 1, &t_slope, &t_intercept, &t_correlation );
+        else 
+            higuchi_dimension( updated_x_coord, y_coord, num_points, 1, interval_min, interval_max, 1, &t_slope, &t_intercept, &t_correlation );
+        
+    }
+    free( updated_x_coord );
     return ( t_correlation > min_correlation );
 }
    
@@ -218,22 +257,22 @@ int main( int argc, char **argv )
     int interval_min, interval_max, extra_output;
     double *values = 0, *xvalues = 0, *yvalues = 0;
     double *ptr;
-    double correlation = 0, dimension, intercept;
+    double slope, intercept, correlation = 0;
     int first_period = 0, last_period = 0, credibility;
         
     if ( argc == 1 ) 
     { 
-        printf( "Selection of algorithm:\n-h: \"canonical\" higuchi f.d;\n-hm: higuchi-style f.d. with variable x coord;\n-p: peng f.d;\n-c,-d: log-periodic approximation;\nOptional keys:\n-xy: data in x-y representation;\n-max: upper limit of intervals in logarithmic dependency, in integer units;\n-v extra output.\nstdin: input data, comma-separated, in one line.\n" ); return 1;
+        printf( "Selection of algorithm:\n-h: \"canonical\" higuchi f.d;\n-hm: higuchi-style f.d. with variable x coord;\n-p,-pm: peng f.d; \n-c,-d: log-periodic approximation;\nOptional keys:\n-xy: data in x-y representation;\n-max: upper limit of intervals in logarithmic dependency, in integer units;\n-v: extra output.\nstdin: input data, comma-separated.\n" ); return 1;
     }
     
     p=buf;
     while ( fread( p, 1, 1, ifile ) == 1 )
     {
-        if ( *p == ',' )
+        if ( *p == ',' || *p == '\n' )
         {
             if ( npoints >= maxnpoints - 1 )
             {
-                maxnpoints += 50;
+                maxnpoints += 1000;
                 ptr = (double*) malloc( maxnpoints * sizeof( double ) );
                 memcpy( ptr, values, npoints * sizeof( double ) );
                 if ( values != 0 ) free( values );
@@ -277,21 +316,21 @@ int main( int argc, char **argv )
         {
             case 'h':
             {
-                higuchi_dimension( xvalues, yvalues, npoints, ( argv[1][2] == 'm' ) ? 1 : 0, interval_min, interval_max, extra_output, &dimension, &intercept, &correlation );
-                printf( "dimension: %g\nintercept: %g\ncorrelation: %g\n", dimension, intercept, correlation );        
+                higuchi_dimension( xvalues, yvalues, npoints, ( argv[1][2] == 'm' ) ? 1 : 0, interval_min, interval_max, extra_output, &slope, &intercept, &correlation );
+                printf( "dimension: %g\nintercept: %g\ncorrelation: %g\n", -slope, intercept, correlation );        
                 break;
             }
             case 'p':
             {
-                peng_dimension( xvalues, yvalues, npoints, interval_min, interval_max, extra_output, &dimension, &intercept, &correlation );
-                printf( "dimension: %g\nintercept: %g\ncorrelation: %g\n", dimension, intercept, correlation );        
+                peng_dimension( xvalues, yvalues, npoints, ( argv[1][2] == 'm' ) ? 1 : 0, interval_min, interval_max, extra_output, &slope, &intercept, &correlation );
+                printf( "dimension: %g\nintercept: %g\ncorrelation: %g\n", -slope, intercept, correlation );        
                 break;
             }
             case 'c':
             case 'd':
             {
-                credibility = logperiodic_approximation( xvalues, yvalues, npoints, argv[1][1] == 'c', interval_min, interval_max, extra_output, &first_period, &last_period, &correlation );
-                printf( "credibility: %s\ndirection: %s\nperiod in beginning: %d\nperiod in end: %d\ncorrelation: %g\n", credibility ? "sufficient" : "insufficient", ( first_period < last_period ) ? "expansion from past" : "contraction to future", first_period, last_period, correlation );          
+                credibility = logperiodic_approximation( xvalues, yvalues, npoints, argv[1][1] == 'c', interval_min, interval_max, extra_output, &first_period, &last_period, &slope, &intercept, &correlation );
+                printf( "credibility: %s\ndirection: %s\nperiod in beginning: %d\nperiod in end: %d\ndimension: %g\nintercept: %g\ncorrelation: %g\n", credibility ? "sufficient" : "insufficient", ( first_period < last_period ) ? "expansion from past" : "contraction to future", first_period, last_period, -slope, intercept, correlation );          
                 break;
             }
             default:
